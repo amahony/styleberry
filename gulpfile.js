c185aa6e75b9c5aa8bf6abdbd9152b7714e5a78e
amahony <atanas@codeberry.io>
2016-10-24 20:39:31 +0300

    adding structure modules

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var atImport = require('postcss-import'),
+    cssnano = require('cssnano'),
+    cssnext = require('postcss-cssnext'),
+    gulp = require('gulp'),
+    postcss = require('gulp-postcss');
+
+var globs = {
+  css: [
+    './src/styleberry.css'
+  ]
+};
+
+gulp.task('css', function() {
+  return gulp
+    .src(globs.css)
+    .pipe(postcss([
+      atImport(),
+      cssnext()
+    ]))
+    .pipe(gulp.dest('dest'));
+});
+
+gulp.task('minify', function() {
+  return gulp
+    .src(globs.css)
+    .pipe(postcss([
+      atImport(),
+      cssnext({warnForDuplicates:false}),
+      cssnano()
+    ]))
+    .pipe(gulp.dest('dest'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/**/*.css', ['css']);
+});
